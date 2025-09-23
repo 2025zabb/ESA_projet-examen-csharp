2bed3e6f668396c131ef177d6f6b6f65ff28ea0c
chnza <nchruit@gmail.com>
2025-09-23 19:48:11 +0200

    first commit

---
+++ src/zabb/program.java
@@ -0,0 +1,31 @@
+package zabb;
+
+import java.util.Scanner;
+
+public class program {
+
+    public static void main(String[] args) {
+        System.out.println("tu veux te presentation : ");
+        Scanner input = new Scanner(System.in);
+        String o = input.nextLine();
+        if(o.equals("o")){
+            presentation();
+        }
+
+    }
+    public static void presentation(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter votre nom : ");
+        String nom = input.nextLine();
+        System.out.println("Please enter votre prenom : ");
+        String prenom = input.nextLine();
+
+        if(!nom.equals(prenom)){
+            System.out.println(" bonjour, je m'appelle :" + nom + " " + prenom);
+        }else {
+            System.out.println(" votre nom : " + nom + " " + prenom +"sont identique");
+        }
+
+        }
+
+}
