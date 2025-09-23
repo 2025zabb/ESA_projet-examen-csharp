444033344719f5903e1095ff4f32079a1964e9c0
chnza <nchruit@gmail.com>
2025-09-23 20:00:03 +0200

    modification/presentation

--- src/zabb/program.java
+++ src/zabb/program.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class program {
 
     public static void main(String[] args) {
-        System.out.println("tu veux te presenter : ");
+        System.out.println("tu veux te presenter(o/n) : ");
         Scanner input = new Scanner(System.in);
         String o = input.nextLine();
         if(o.equals("o")){
@@ -21,11 +21,10 @@ public class program {
         String prenom = input.nextLine();
 
         if(!nom.equals(prenom)){
-            System.out.println(" bonjour, je m'appelle :" + nom + " " + prenom);
+            System.out.println("bonjour, je m'appelle " + nom + " " + prenom);
         }else {
             System.out.println(" votre nom : " + nom + " " + prenom +"sont identique");
         }
 
         }
-
 }
