e0465408e4907e66c7c269134d4a5e383d661eca
chnza <nchruit@gmail.com>
2025-09-23 20:43:16 +0200

    amélioration du code

--- src/zabb/program.java
+++ src/zabb/program.java
@@ -5,26 +5,42 @@ import java.util.Scanner;
 public class program {
 
     public static void main(String[] args) {
-        System.out.println("tu veux te presenter(o/n) : ");
         Scanner input = new Scanner(System.in);
+
+        System.out.println("tu veux te presenter(o/n) : ");
         String o = input.nextLine();
+
         if(o.equals("o")){
-            presentation();
+            presentation(input);
+        }else  if(o.equals("n")){
+            System.out.println("au revoir");
+            System.exit(0);
+        }else {
+            System.out.println("Choix invalide !");
         }
+        input.close();
 
     }
-    public static void presentation(){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Please enter votre nom : ");
+    public static void presentation(Scanner input){
+        System.out.println("Please enter votre Nom : ");
         String nom = input.nextLine();
-        System.out.println("Please enter votre prenom : ");
+        System.out.println("Please enter votre Prenom : ");
         String prenom = input.nextLine();
+        System.out.println("Please enter votre Hobby : ");
+        String hobby = input.nextLine();
+
+
 
         if(!nom.equals(prenom)){
-            System.out.println("bonjour, je m'appelle " + nom + " " + prenom);
+            System.out.println("bonjour, je m'appelle " + nom + " " + prenom+".");
         }else {
             System.out.println(" votre nom : " + nom + " " + prenom +"sont identique");
         }
-
+        if (hobby.equals(" ")){
+            System.out.println(" votre Hobby !?");
+        }else{
+            System.out.println("j'aime" +" "+ hobby+".");
         }
+
+    }
 }
